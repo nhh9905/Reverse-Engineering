21509e21502f383de4da26104ed484c9002a1621
nhh9905 <hoanglclvt@gmail.com>
2024-10-15 10:02:47 +0700

    update

---
+++ Windows API/Memory Management/ex3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <filesystem>
+using namespace std;
+namespace fs = filesystem;
+int main() {
+    freopen("a.inp", "r", stdin);
+    freopen("a.out", "w", stdout);
+    string path;
+    getline(cin, path);
+    for (char x:path) {
+        if (x == '\\')
+            x = '/';
+    }
+    if (fs::exists(path) && fs::is_directory(path)) {
+        cout << "File in directory " << path << endl;
+        for (const auto& entry:fs::directory_iterator(path))
+            cout << entry.path().filename().string() << endl;
+    }
+    else
+        cout << "Directory does not exist or is not accessible";
+}
